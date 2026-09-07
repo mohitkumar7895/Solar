ad58554ebf94a5d4b8e3f66e16aaf45f5ed1e21c
mohitkumar7895 <mohitporwal596@gmail.com>
2026-09-07 11:00:01 +0530

    rr

--- powerupsolarengineering.com/js/site-data-manager.js
+++ powerupsolarengineering.com/js/site-data-manager.js
@@ -9,7 +9,7 @@
  * 6. Smooth Scroll Reveal Animations
  * 7. Hybrid Server Sync (data-api.php)
  */
-const API_ENDPOINT = "/api/data-api.js";
+const API_ENDPOINT = "/api/data-api";
 
 // =========================================================================
 // 0. THEME SWITCHER STUDIO & SYSTEM
